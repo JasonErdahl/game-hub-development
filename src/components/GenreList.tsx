823e4cbdf610e7ec377794d470f888073b56b390
Jason Erdahl <jason.erdahl@marvin.com>
2024-04-15 12:35:41 -0500

    Genre Update

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -25,7 +25,7 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   return (
     <>
       <Heading fontSize="2xl" marginBottom={3}>
-        Genres
+        Genres123
       </Heading>
       <List>
         {data.map((genre) => (
